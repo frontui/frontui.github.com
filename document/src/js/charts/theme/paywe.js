a7e532cdf6f479b4cf89bee09d65cfe4331001cd
tomieric <tomieric@gmail.com>
2016-03-25 10:36:11 +0800

    update

--- document/src/js/charts/theme/paywe.js
+++
@@ -1,7 +0,0 @@
-/*! 
-*  frontui v1.0.2
-*  by frontpay FE Team
-*  (c) 2016-03-25 www.frontpay.cn
-*  Licensed under Apache License
-*/
-define("echarts/theme/paywe",function(){var e={color:["#689eec","#d1d1d1","#b6a2de","#fd8832","#54d393","#de5252","#b5c9e5","#8fb0db"],title:{show:!1},grid:{backgroundColor:"#f9f9f9",borderColor:"#e6e5e5",borderWidth:0,y:20,x2:20,y2:60},tooltip:{textStyle:{color:"#666666",fontFamily:"\u5b8b\u4f53,simsun,Verdana, sans-serif",fontSize:12},padding:[10,30,10,20],borderWidth:1,borderColor:"#e6e6e6",backgroundColor:"rgba(255,255,255,0.7)",axisPointer:{type:"line",lineStyle:{width:1,color:"#008acd"},crossStyle:{color:"#008acd"},shadowStyle:{color:"rgba(200,200,200,0.2)"}},trigger:"axis"},legend:{data:["undefined"],y:"bottom",textStyle:{fontSize:12,fontFamily:"Tohama,Microsoft Yahei",color:"#777"}},toolbox:{show:!1},xAxis:[{type:"category",boundaryGap:!1,data:["undefined"],axisLabel:{textStyle:{fontSize:11,fontFamily:"Myriad Pro Regular,Tohama,Microsoft Yahei",color:"#666666"}},axisLine:{lineStyle:{width:0,type:"solid",color:"#cccccc"}},axisTick:{show:!1},splitLine:{show:!1}}],yAxis:[{type:"value",axisLine:{lineStyle:{width:0,type:"solid",color:"#e6e5e5"}},axisLabel:{textStyle:{color:"#999999"}},axisTick:{show:!1},splitLine:{lineStyle:{width:1,type:"solid",color:["#e6e5e5"]}}}],series:[],lineStyle:{color:"#eeeeee"}};return e});
