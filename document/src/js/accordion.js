0a71cbd690ac3b634ccff783b7199e4d57656cff
tomieric <tomieric@gmail.com>
2016-05-17 09:45:23 +0800

    update

--- document/src/js/accordion.js
+++ document/src/js/accordion.js
@@ -1,7 +1,7 @@
 /*! 
 *  frontui v1.0.2
 *  by frontpay FE Team
-*  (c) 2016-05-10 www.frontpay.cn
+*  (c) 5/17/2016 www.frontpay.cn
 *  Licensed under Apache License
 */
-!function(e,t){"function"==typeof define&&define.amd?define("ui/according",["jquery","ui/switcher"],t):"object"==typeof exports?module.exports=t(require("jquery")):t(e.jQuery)}(this,function(e){"use strict";return e.fn.accordion=function(t){var i={item:"li",target:">div",active:"active",except:!1};return t=e.extend({},i,t),e(this).switcher(t),e(this).each(function(){var i=e(this).find(t.item);e(this).on("select.ui.switcher",function(n){var r=e(n.relatedTarget),c=r.hasClass(t.active),o=i.find(t.target),s=r.find(t.target);!t.except&&o.slideUp(),s.stop()[c?"slideDown":"slideUp"](),n.stopPropagation(),n.preventDefault()})})},e});
+"use strict";$.fn.accordion=function(t){var e={item:"li",target:">div",active:"active",except:!1};return t=$.extend({},e,t),$(this).switcher(t),$(this).each(function(){var e=$(this).find(t.item);$(this).on("select.ui.switcher",function(i){var a=$(i.relatedTarget),n=a.hasClass(t.active),s=e.find(t.target),r=a.find(t.target);!t.except&&s.slideUp(),r.stop()[n?"slideDown":"slideUp"](),i.stopPropagation(),i.preventDefault()})})};
--- document/src/js/ui/transition.js
+++
@@ -1,7 +0,0 @@
-/*! 
-*  frontui v1.0.2
-*  by frontpay FE Team
-*  (c) 2016-03-25 www.frontpay.cn
-*  Licensed under Apache License
-*/
-!function(n,t){"function"==typeof define&&define.amd?define("ui/transition",["jquery"],t):"object"==typeof exports?module.exports=t(require("jquery")):t(n.jQuery)}(this,function(n){"use strict";function t(){var n=document.createElement("ui"),t={WebkitTransition:"webkitTransitionEnd",MozTransition:"transitionend",OTransition:"oTransitionEnd otransitionend",transition:"transitionend"};for(var i in t)if(void 0!==n.style[i])return{end:t[i]};return!1}return n.fn.emulateTransitionEnd=function(t){var i=!1,e=this;n(this).one("uiTransitionEnd",function(){i=!0});var r=function(){i||n(e).trigger(n.support.transition.end)};return setTimeout(r,t),this},n(function(){n.support.transition=t(),n.support.transition&&(n.event.special.uiTransitionEnd={bindType:n.support.transition.end,delegateType:n.support.transition.end,handle:function(t){return n(t.target).is(this)?t.handleObj.handler.apply(this,arguments):void 0}})}),n});
